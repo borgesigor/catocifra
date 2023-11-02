e06a0ef1f62666ebd4547b0f2f5079d58254b130
fivesev <igoregiovanna@gmail.com>
2023-11-01 22:45:50 -0300

    clear test modules

--- src/api/external/HttpAdapter.ts
+++
@@ -1,9 +0,0 @@
-// import { Router } from 'express';
-// const router = Router();
-// import IHttpContext from './IHttpContext';
-
-// export default class ExpressAdapter implements IHttpContext {
-//   get(path: any, arg: any) {
-//     router.get()
-//   }
-// }
