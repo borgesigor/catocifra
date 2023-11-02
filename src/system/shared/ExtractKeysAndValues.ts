397902e9450ec87ea819b39278be1383fe30e523
fivesev <igoregiovanna@gmail.com>
2023-11-01 22:40:56 -0300

    remove non utilized folders

--- src/system/shared/ExtractKeysAndValues.ts
+++
@@ -1,6 +0,0 @@
-export default function extractKeysAndValues(data: Object): { chaves: string, valores: string } {
-  const chaves = Object.keys(data).map(chave => `"${chave}"`).join(', ');
-  const valores = Object.values(data).map(valor => `'${valor}'`).join(', ');
-
-  return { chaves, valores };
-}
